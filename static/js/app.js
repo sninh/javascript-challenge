626f7b16c45ee1d593cae3df616a3b99b2c388ef
sninh <stevenninh@gmail.com>
2021-07-13 21:43:35 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -7,35 +7,26 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
-console.log(data);
-
-// // Step 1: Loop Through `data` and console.log each UFO Sighting
-tableData.forEach(function(sighting) {
-    console.log(sighting);
- });
-
 // // Step 2:  Use d3 to append one table row `tr` for each UFO Sighting
 // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
 tableData.forEach(function(sighting) {
-   console.log(sighting);
-   var row = tbody.append("tr");
- });
-
-// // Step 3:  Use `Object.entries` to console.log each UFO Sighting
-tableData.forEach(function(sighting) {
-   console.log(sighting);
+   //console.log(sighting);
    var row = tbody.append("tr");
 
    Object.entries(sighting).forEach(function([key, value]) {
-     console.log(key, value);
-   });
- });
+    var cell = row.append("td")
+    cell.text(value);
+  });
+});
 
 var button = d3.select("#filter-btn");
 
+
 // Select the form 
 var form = d3.select(".form-group");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
 // Create an eventlistener to filter the table.
 function runEnter() {
@@ -51,8 +42,7 @@ function runEnter() {
   // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
-    console.log(inputvalue);
-    console.log(tableData);
+    //console.log(inputvalue);
 
     
     var filteredInput = tableData.filter(data => data.datetime === inputValue);
